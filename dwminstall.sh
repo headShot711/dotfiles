4f4230334635f812c70569982bd4d276d9b8e236
headShot <headShot711@protonmail.com>
2023-07-06 08:55:09 +0530

    Update dwminstall.sh

--- dwminstall.sh
+++ dwminstall.sh
@@ -4,13 +4,17 @@
 sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 sudo pacman -Syyu --noconfirm
+sudo pacman -S --noconfirm --needed libx11
 sudo pacman -S --noconfirm --needed xlib
-sudo pacman -S --noconfirm --needed xorg
-sudo pacman -S --noconfirm --needed xorg-server
-sudo pacman -S --noconfirm --needed xorg-xinit
-sudo pacman -S --noconfirm --needed xorg-utils
-sudo pacman -S --noconfirm --needed xorg-xinit
-sudo pacman -S --noconfirm --needed xorg-server-utils
+sudo pacman -S --noconfirm --needed base-devel
+sudo pacman -S --noconfirm --needed freetype2
+sudo pacman -S --noconfirm --needed fontconfig
+#sudo pacman -S --noconfirm --needed xorg
+#sudo pacman -S --noconfirm --needed xorg-server
+#sudo pacman -S --noconfirm --needed xorg-xinit
+#sudo pacman -S --noconfirm --needed xorg-utils
+#sudo pacman -S --noconfirm --needed xorg-xinit
+#sudo pacman -S --noconfirm --needed xorg-server-utils
 sudo pacman -S --noconfirm --needed libxft
 sudo pacman -S --noconfirm --needed libxinerama
 sudo pacman -S --noconfirm --needed vim
@@ -24,29 +28,29 @@ sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 sudo pacman -S --noconfirm --needed xdotool
 sudo pacman -S --noconfirm --needed neofetch
 sudo pacman -S --noconfirm --needed htop
-sudo pacman -S --noconfirm --needed unzip
-sudo pacman -S --noconfirm --needed p7zip
-sudo pacman -S --noconfirm --needed pulseaudio
-sudo pacman -S --noconfirm --needed pulseaudio-bluetooth
-sudo pacman -S --noconfirm --needed alsa-utils
-sudo pacman -S --noconfirm --needed pavucontrol
-sudo pacman -S --noconfirm --needed polkit-gnome
-sudo pacman -S --noconfirm --needed mpv
-sudo pacman -S --noconfirm --needed pcmanfm
-sudo pacman -S --noconfirm --needed ranger
-sudo pacman -S --noconfirm --needed gvfs
-sudo pacman -S --noconfirm --needed gvfs-mtp
-sudo pacman -S --noconfirm --needed ntfs-3g
-sudo pacman -S --noconfirm --needed bluez
-sudo pacman -S --noconfirm --needed bluez-utils
-sudo pacman -S --noconfirm --needed blueman
-sudo pacman -S --noconfirm --needed networkmanager
-sudo pacman -S --noconfirm --needed network-manager-applet
-sudo pacman -S --noconfirm --needed dhcpcd
-sudo pacman -S --noconfirm --needed wpa_supplicant
-sudo pacman -S --noconfirm --needed xdg-utils
-sudo pacman -S --noconfirm --needed curl
-sudo pacman -S --noconfirm --needed leafpad
+#sudo pacman -S --noconfirm --needed unzip
+#sudo pacman -S --noconfirm --needed p7zip
+#sudo pacman -S --noconfirm --needed pulseaudio
+#sudo pacman -S --noconfirm --needed pulseaudio-bluetooth
+#sudo pacman -S --noconfirm --needed alsa-utils
+#sudo pacman -S --noconfirm --needed pavucontrol
+#sudo pacman -S --noconfirm --needed polkit-gnome
+#sudo pacman -S --noconfirm --needed mpv
+#sudo pacman -S --noconfirm --needed pcmanfm
+#sudo pacman -S --noconfirm --needed ranger
+#sudo pacman -S --noconfirm --needed gvfs
+#sudo pacman -S --noconfirm --needed gvfs-mtp
+#sudo pacman -S --noconfirm --needed ntfs-3g
+#sudo pacman -S --noconfirm --needed bluez
+#sudo pacman -S --noconfirm --needed bluez-utils
+#sudo pacman -S --noconfirm --needed blueman
+#sudo pacman -S --noconfirm --needed networkmanager
+#sudo pacman -S --noconfirm --needed network-manager-applet
+#sudo pacman -S --noconfirm --needed dhcpcd
+#sudo pacman -S --noconfirm --needed wpa_supplicant
+#sudo pacman -S --noconfirm --needed xdg-utils
+#sudo pacman -S --noconfirm --needed curl
+#sudo pacman -S --noconfirm --needed leafpad
 
 cd
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
