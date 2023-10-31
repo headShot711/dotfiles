e753fc4650aa5316f0bc564bffb96e3e7756b200
headShot <headShot711@protonmail.com>
2023-10-31 20:51:42 +0530

    Update install.sh

--- qtile/install.sh
+++ qtile/install.sh
@@ -14,7 +14,7 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Packages to install from Arch Linux official repos
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance nitrogen picom curl base-devel linux-headers vim htop neofetch dunst xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot thunar feh alacritty firefox polkit-gnome exa qtile lightdm lightdm-gtk-greeter python-psutil cmus mpv lightdm lightdm-gtk-greeter
+sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance nitrogen picom curl base-devel linux-headers vim htop neofetch dunst xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot thunar feh alacritty firefox polkit-gnome exa qtile python-psutil cmus mpv lightdm lightdm-gtk-greeter
 
 # Install XFCE
 #sudo pacman -S --noconfirm --needed xfce4
@@ -24,12 +24,12 @@ cd
 sudo cp -rv ~/dotfiles/qtile/config.py ~/.config/qtile/
 sudo cp -rv ~/dotfiles/qtile/autostart.sh ~/.config/qtile/
 sudo cp -rv ~/dotfiles/qtile/alacritty.yml ~/.config/alacritty/
-sudo cp -rv ~/dotfiles/qtile/picom.conf ~/.config/picom/
+#sudo cp -rv ~/dotfiles/qtile/picom.conf ~/.config/picom/
 sudo cp -rv ~/dotfiles/walls/* ~/walls/
 sudo cp -rv ~/dotfiles/qtile/.bashrc ~/
 cd .config/qtile
 sudo chmod +x autostart.sh
-
+cd
 
 # Enable services
 sudo systemctl enable lightdm.service
