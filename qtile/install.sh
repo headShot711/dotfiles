e636435b424e5a491d9a80e7143401a36a7750e0
headShot <headShot711@protonmail.com>
2023-10-31 16:43:23 +0530

    Update install.sh

--- qtile/install.sh
+++ qtile/install.sh
@@ -14,7 +14,7 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Packages to install from Arch Linux official repos
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps go lxappearance nitrogen picom curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g vim htop neofetch tlp lm_sensors yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot mpv thunar feh alacritty firefox vim polkit-gnome qbittorrent libvirt qemu-full iptables-nft dnsmasq virt-manager qemu-arch-extra ovmf vde2 ebtables bridge-utils openbsd-netcat
+sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps go lxappearance nitrogen picom curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g vim htop neofetch tlp lm_sensors yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot mpv thunar feh alacritty firefox vim polkit-gnome qbittorrent libvirt qemu-full iptables-nft dnsmasq virt-manager qemu-arch-extra ovmf vde2 ebtables bridge-utils openbsd-netcat exa qtile
 sudo pacman -S --noconfirm 
 
 # Install XFCE
@@ -36,6 +36,7 @@ sudo cp -rv ~/dotfiles/qtile/config.py ~/.config/qtile/
 sudo cp -rv ~/dotfiles/qtile/autostart.sh ~/.config/qtile/
 sudo cp -rv ~/dotfiles/qtile/alacritty.yml ~/.config/alacritty/
 sudo cp -rv ~/dotfiles/qtile/picom.conf ~/.config/picom/
+sudo cp -rv ~/dotfiles/qtile/.bashrc ~/
 
 # Enable services
 sudo systemctl enable bluetooth.service
