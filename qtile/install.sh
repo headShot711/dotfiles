b3350f5570f607b80f90461d895272bca8b0a5d4
headShot <headShot711@protonmail.com>
2023-11-01 13:14:52 +0530

    Update install.sh

--- qtile/install.sh
+++ qtile/install.sh
@@ -13,7 +13,7 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Packages to install from Arch Linux official repos
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance nitrogen picom curl base-devel linux-headers vim htop neofetch dunst xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot thunar feh alacritty firefox polkit-gnome exa qtile python-psutil cmus lightdm lightdm-gtk-greeter bluez bluez-utils blueman dhcpcd yt-dlp mtools dosfstools exfat-utils gvfs gvfs-mtp ntfs-3g tlp qbittorrent libvirt qemu-full iptables-nft dnsmasq virt-manager qemu-arch-extra ovmf vde2 ebtables bridge-utils openbsd-netcat vlc tor
+sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance nitrogen picom curl base-devel linux-headers vim htop neofetch dunst xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot thunar feh alacritty firefox polkit-gnome exa qtile python-psutil cmus lightdm lightdm-gtk-greeter bluez bluez-utils blueman dhcpcd yt-dlp mtools dosfstools exfat-utils gvfs gvfs-mtp ntfs-3g tlp qbittorrent libvirt qemu-full iptables-nft dnsmasq virt-manager qemu-arch-extra ovmf vde2 ebtables bridge-utils openbsd-netcat vlc
 
 # Install XFCE
 sudo pacman -S --noconfirm --needed xfce4
@@ -33,7 +33,6 @@ cd
 sudo systemctl enable lightdm.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable libvirtd.service
-sudo systemctl enable tor.service
 
 clear
 echo "DONE!"
