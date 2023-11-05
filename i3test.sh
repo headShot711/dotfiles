4e30f7a12bfda63739a101b11ec37f08b304d3cc
headShot <headShot711@protonmail.com>
2023-11-05 11:47:15 +0530

    Update i3test.sh

--- i3test.sh
+++ i3test.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Make directories
 cd
 mkdir -v walls
@@ -11,21 +9,74 @@ mkdir -v ~/.config/polybar
 #Add parallel downloading
 sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-#Use reflector to setup fastest mirrors
-sudo pacman -S --noconfirm --needed reflector rsync curl
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-
 # Packages to install from Arch Linux official repos
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed xorg-xrandr xorg-xbacklight lxappearance nitrogen picom i3-gaps i3status i3lock base-devel tor mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch lm_sensors yt-dlp rofi polybar dunst xclip unzip p7zip networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot mpv thunar feh alacritty firefox vim  polkit-gnome qbittorrent bluez bluez-utils vlc
+sudo pacman -S --noconfirm --needed xorg  
+sudo pacman -S --noconfirm --needed go
+sudo pacman -S --noconfirm --needed lxappearance
+sudo pacman -S --noconfirm --needed nitrogen
+sudo pacman -S --noconfirm --needed picom
+sudo pacman -S --noconfirm --needed i3-gaps
+sudo pacman -S --noconfirm --needed i3status
+sudo pacman -S --noconfirm --needed i3lock
+sudo pacman -S --noconfirm --needed curl
+sudo pacman -S --noconfirm --needed base-devel
+sudo pacman -S --noconfirm --needed mtools
+sudo pacman -S --noconfirm --needed dosfstools
+sudo pacman -S --noconfirm --needed exfat-utils
+sudo pacman -S --noconfirm --needed linux-headers
+sudo pacman -S --noconfirm --needed gvfs
+sudo pacman -S --noconfirm --needed gvfs-mtp
+sudo pacman -S --noconfirm --needed ntfs-3g
+sudo pacman -S --noconfirm --needed htop
+sudo pacman -S --noconfirm --needed neofetch
+sudo pacman -S --noconfirm --needed tlp
+sudo pacman -S --noconfirm --needed yt-dlp
+sudo pacman -S --noconfirm --needed rofi
+sudo pacman -S --noconfirm --needed dunst
+sudo pacman -S --noconfirm --needed xclip
+sudo pacman -S --noconfirm --needed unzip
+sudo pacman -S --noconfirm --needed p7zip
+sudo pacman -S --noconfirm --needed bluez
+sudo pacman -S --noconfirm --needed bluez-utils
+sudo pacman -S --noconfirm --needed blueman
+sudo pacman -S --noconfirm --needed networkmanager
+sudo pacman -S --noconfirm --needed network-manager-applet
+sudo pacman -S --noconfirm --needed dhcpcd
+sudo pacman -S --noconfirm --needed wpa_supplicant
+sudo pacman -S --noconfirm --needed xdg-utils
+sudo pacman -S --noconfirm --needed xdg-user-dirs
+sudo pacman -S --noconfirm --needed alsa-utilssudo pacman -S --noconfirm --needed pavucontrol
+sudo pacman -S --noconfirm --needed ranger
+sudo pacman -S --noconfirm --needed leafpad  
+sudo pacman -S --noconfirm --needed ttf-liberation
+sudo pacman -S --noconfirm --needed scrot
+sudo pacman -S --noconfirm --needed vlc
+sudo pacman -S --noconfirm --needed thunar 
+sudo pacman -S --noconfirm --needed feh
+sudo pacman -S --noconfirm --needed alacritty
+sudo pacman -S --noconfirm --needed firefox
+sudo pacman -S --noconfirm --needed vim 
+sudo pacman -S --noconfirm --needed polkit-gnome
+sudo pacman -S --noconfirm --needed qbittorrent      
+sudo pacman -S --noconfirm --needed libvirt
+sudo pacman -S --noconfirm --needed qemu-full
+sudo pacman -S --force --needed iptables-nft
+sudo pacman -S --noconfirm --needed dnsmasq
+sudo pacman -S --noconfirm --needed virt-manager
+sudo pacman -S --noconfirm --needed qemu-arch-extra
+sudo pacman -S --noconfirm --needed ovmf
+sudo pacman -S --noconfirm --needed vde2
+sudo pacman -S --noconfirm --needed ebtables
+sudo pacman -S --noconfirm --needed bridge-utils
+sudo pacman -S --noconfirm --needed openbsd-netcat
 
 # Install XFCE
-sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies
+#sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies
 
 # Install yay
-sudo pacman -S --noconfirm --needed go
+
 cd
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -33,7 +84,8 @@ makepkg -si
 
 # Install yay packages
 cd
-yay -S --noconfirm --needed autotiling ly brave-bin
+yay -S --noconfirm autotiling
+yay -S --noconfirm ly
 
 # Copy config files
 cd
@@ -43,16 +95,10 @@ sudo cp -rv ~/dotfiles/polybar/* ~/.config/polybar/
 sudo cp -rv ~/dotfiles/walls/* ~/walls/
 sudo chmod +x ~/.config/polybar/launch.sh
 
-# Setup Virtual Box
-sudo pacman -S --noconfirm virtualbox virtualbox-host-modules-arch qt5-x11extras
-yay -S --noconfirm virtualbox-ext-oracle
-sudo modprobe vboxdrv
-sudo gpasswd -a hs vboxusers
-
 # Enable services
 sudo systemctl enable bluetooth.service
+sudo systemctl enable libvirtd.service
 sudo systemctl enable ly.service
-sudo systemctl enable tor.service
 
 # Most important command! Get rid of the beep!
 sudo -i
