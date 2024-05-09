4830d32a9cdbb2b219e89dfbea8ca653d048beca
headShot <headShot711@protonmail.com>
2024-05-09 16:25:49 +0530

    Update obox.sh

--- obox.sh
+++ obox.sh
@@ -4,7 +4,7 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Install packages
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed vim firefox curl xorg xorg-xrandr lightdm lightdm-gtk-greeter xfce4 openbox obconf tint2 lxterminal lxappearance nitrogen rofi yad screen pluma nemo ranger leafpad linux-headers htop neofetch wget base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol alacritty firefox chromium mate-polkit exa
+sudo pacman -S --noconfirm --needed vim firefox curl xorg xorg-xrandr lightdm lightdm-gtk-greeter xfce4 openbox obconf tint2 menumaker lxterminal lxappearance nitrogen rofi yad screen ranger leafpad linux-headers htop neofetch wget base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol firefox chromium mate-polkit exa
 
 # Set bash alias
 cd
