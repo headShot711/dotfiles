2e1f145c8273d825dd2802d7474b9886d1dfcd01
headShot <headShot711@protonmail.com>
2024-05-10 21:06:56 +0530

    Update minimal_i3.sh

--- minimal_i3.sh
+++ minimal_i3.sh
@@ -1,1 +1,62 @@
+# Make directories
+cd
+mkdir -v walls
+mkdir -v ~/.config
+mkdir -v ~/.config/alacritty
+mkdir -v ~/.config/i3
+mkdir -v ~/.config/i3status
 
+# Add parallel downloading
+sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+
+# Install packages
+
+sudo pacman -Syyu --noconfirm
+sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen picom i3-wm i3status i3lock kate dmenu curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger ttf-liberation scrot pcmanfm feh firefox vim polkit-gnome wget exa
+
+#libvirt qemu-full iptables-nft dnsmasq virt-manager ovmf vde2 ebtables bridge-utils openbsd-netcat
+
+# Install st terminal
+cd
+git clone https://git.suckless.org/st
+cd st
+sudo make clean install
+
+
+# Install yay
+cd
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install yay packages
+cd
+yay -S --noconfirm autotiling
+yay -S --noconfirm ly
+
+# Install font
+cd
+wget https://github.com/be5invis/Iosevka/releases/download/v27.3.4/super-ttc-iosevka-27.3.4.zip
+unzip super-ttc-iosevka-27.3.4.zip
+sudo mv iosevka.ttc /usr/share/fonts/
+sudo fc-cache
+rm -f super-ttc-iosevka-27.3.4.zip
+
+# Set bash alias
+cd
+echo "alias ls='exa -lah'" >> .bashrc
+
+# Copy config files
+#cd
+#sudo cp -rv ~/dotfiles/i3/alacritty.yml ~/.config/alacritty/
+#sudo cp -rv ~/dotfiles/i3/config ~/.config/i3/config
+#sudo cp -rv ~/dotfiles/i3/i3status.conf ~/.config/i3status/config
+#sudo cp -rv ~/dotfiles/walls/* ~/walls/
+
+# Enable services
+sudo systemctl enable libvirtd.service
+sudo systemctl enable ly.service
+sudo systemctl enable bluetooth.service
+
+clear
+echo "DONE!"
