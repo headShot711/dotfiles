86825c2aa1a551e7aa5223f02446f88966766dab
headShot <headShot711@protonmail.com>
2024-06-06 18:16:20 +0530

    Update debi3minimal.sh

--- debi3minimal.sh
+++ debi3minimal.sh
@@ -35,13 +35,6 @@ sudo apt install -y lxappearance
 # Browsers
 sudo apt install -y firefox-esr 
 
-# Install Brave Browser
-sudo apt install curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
-
 # Desktop background browser/handler 
 # feh --bg-fill /path/to/directory 
 # sudo apt install -y nitrogen 
