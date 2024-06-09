1f0512f01f952659281548a0e3d425dd11b0fca8
headShot <headShot711@protonmail.com>
2024-06-09 14:04:29 +0530

    Update install.sh

--- i3-dark/install.sh
+++ i3-dark/install.sh
@@ -5,6 +5,7 @@ mkdir -v ~/.config
 mkdir -v ~/.config/alacritty
 mkdir -v ~/.config/i3
 mkdir -v ~/.config/i3status
+mkdir -v ~/.config/i3blocks
 
 # Add parallel downloading
 sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
@@ -12,7 +13,7 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 # Install packages
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen cmus picom evince i3-wm i3status timeshift neovim i3lock i3blocks dmenu curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger leafpad ttf-liberation scrot vlc thunar feh alacritty firefox vim polkit-gnome wget qbittorrent eza
+sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen cmus picom evince acpi i3-wm i3status timeshift neovim i3lock i3blocks dmenu curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger leafpad ttf-liberation scrot vlc thunar feh alacritty firefox vim polkit-gnome wget qbittorrent eza
 
 # Install yay
 
@@ -38,11 +39,17 @@ rm -f super-ttc-iosevka-27.3.4.zip
 cd
 echo "alias ls='eza -lah'" >> .bashrc
 
+# Install i3blocks scripts
+cd ~/.config/i3blocks
+git clone https://github.com/vivien/i3blocks-contrib.git
+mv i3blocks-contrib scripts
+
 # Copy config files
 cd
 sudo cp -rv ~/dotfiles/i3-dark/alacritty.yml ~/.config/alacritty/
 sudo cp -rv ~/dotfiles/i3-dark/config ~/.config/i3/config
 sudo cp -rv ~/dotfiles/i3-dark/i3status.conf ~/.config/i3status/config
+sudo cp -rv ~/dotfiles/i3-dark/i3blocks/config ~/.config/i3blocks/config
 sudo cp -rv ~/dotfiles/walls/* ~/walls/
 
 # Enable services
