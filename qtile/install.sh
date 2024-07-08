2ad8caa278bda6eda32ec030605dcae26d6f3648
headShot <headShot711@protonmail.com>
2024-07-08 20:01:49 +0530

    Update install.sh

--- qtile/install.sh
+++ qtile/install.sh
@@ -16,9 +16,13 @@ sudo pacman -Syyu --noconfirm
 #sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance pavucontrol nitrogen picom curl base-devel linux-headers vim htop vlc mpv neofetch xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot dolphin feh alacritty firefox polkit-gnome exa qtile python-psutil lightdm lightdm-gtk-greeter bluez bluez-utils blueman yt-dlp qtile
 sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen cmus noto-fonts noto-fonts-emoji noto-fonts-extra picom evince jq pavucontrol keepassxc dmidecode acpi qtile brightnessctl volumeicon flameshot timeshift dmenu curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger leafpad ttf-liberation scrot vlc mpv pcmanfm feh alacritty firefox vim polkit-gnome wget qbittorrent eza
 
-
-# Install XFCE
-#sudo pacman -S --noconfirm --needed xfce4
+# Install font
+cd
+wget https://github.com/be5invis/Iosevka/releases/download/v27.3.4/super-ttc-iosevka-27.3.4.zip
+unzip super-ttc-iosevka-27.3.4.zip
+sudo mv iosevka.ttc /usr/share/fonts/
+sudo fc-cache
+rm -f super-ttc-iosevka-27.3.4.zip
 
 # Copy config files
 cd
