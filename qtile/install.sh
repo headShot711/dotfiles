d75a7052597304d3de1ecaec4abf84c49da5d977
headShot <headShot711@protonmail.com>
2024-07-08 20:19:05 +0530

    Update install.sh

--- qtile/install.sh
+++ qtile/install.sh
@@ -14,7 +14,11 @@ sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 sudo pacman -Syyu --noconfirm
 #sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-apps lxappearance pavucontrol nitrogen picom curl base-devel linux-headers vim htop vlc mpv neofetch xclip networkmanager network-manager-applet alsa-utils ranger leafpad ttf-liberation ttf-font-awesome scrot dolphin feh alacritty firefox polkit-gnome exa qtile python-psutil lightdm lightdm-gtk-greeter bluez bluez-utils blueman yt-dlp qtile
-sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen cmus noto-fonts noto-fonts-emoji noto-fonts-extra picom evince jq pavucontrol keepassxc dmidecode acpi qtile brightnessctl volumeicon flameshot timeshift dmenu curl base-devel mtools dosfstools exfat-utils linux-headers gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger leafpad ttf-liberation scrot vlc mpv pcmanfm feh alacritty firefox vim polkit-gnome wget qbittorrent eza
+sudo pacman -S --noconfirm --needed xorg go lxappearance nitrogen cmus noto-fonts noto-fonts-emoji noto-fonts-extra picom evince jq pavucontrol 
+keepassxc dmidecode acpi qtile brightnessctl volumeicon flameshot timeshift dmenu curl base-devel mtools dosfstools exfat-utils linux-headers 
+gvfs gvfs-mtp ntfs-3g htop neofetch tlp yt-dlp dunst xclip unzip p7zip bluez bluez-utils blueman networkmanager network-manager-applet dhcpcd 
+wpa_supplicant xdg-utils xdg-user-dirs alsa-utils pavucontrol ranger leafpad ttf-liberation scrot vlc mpv pcmanfm feh alacritty firefox vim 
+polkit-gnome wget qbittorrent eza lightdm lightdm-gtk-greeter
 
 # Install font
 cd
