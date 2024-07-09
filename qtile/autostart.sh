5e9bcc624cc586c6fcbe0ff578f4eaaede3f21a1
headShot <headShot711@protonmail.com>
2024-07-09 19:39:56 +0530

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,6 +2,6 @@
 xrandr -s 1920x1080
 feh --bg-scale /home/hs/walls/tn-glados-better.jpg
 nm-applet &
-nitrogen --restore &
+flameshot &
 picom &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
