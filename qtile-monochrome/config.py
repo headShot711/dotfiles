d0aa0003525f9348f4102be9d62327916ff0dccf
headShot <headShot711@protonmail.com>
2024-07-12 17:47:33 +0530

    Update config.py

--- qtile-monochrome/config.py
+++ qtile-monochrome/config.py
@@ -47,22 +47,15 @@ keys = [
     Key([mod], "m", lazy.spawn("alacritty -e cmus"), desc="Launch text music player"),
     Key([mod], "c", lazy.spawn("alacritty -e calcurse"), desc="Open Calendar"),
     Key([mod], "p", lazy.spawn("keepassxc"), desc="Open Password manager"),
-
-    # POWEROFF MENU
     Key([mod], "y", lazy.spawn(os.path.expanduser("~/.config/qtile/power_menu.sh")), desc="Launch the power menu."),
-
-    # AUDIO
     Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +5%"), desc='Volume Up'),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -5%"), desc='volume down'),
     Key([], "XF86AudioMute", lazy.spawn("pulsemixer --toggle-mute"), desc='Volume Mute'),
-
-    # BRIGHTNESS
     Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s 10%+"), desc='brightness UP'),
     Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 10%-"), desc='brightness Down'),
-
-    # DMENU
     Key([mod], "r", lazy.spawn("dmenu_run -nf '#D3D3D3' -nb '#222222' -sb '#36454F' -sf '#D3D3D3' -fn 'Monofur Nerd Font Mono'-10"), desc="Spawn a command using a prompt widget"),
-    
+    Key([mod], "s", lazy.spawn("flameshot gui"), desc='Screenshot'),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
